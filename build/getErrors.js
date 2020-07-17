24204fc4e1b6c61f99ab75c6b5971863191322af
Петр Кленкин <pk@webpractik.ru>
2020-07-17 17:56:13 +0300

    запрос ошибок из каталога

---
+++ build/getErrors.js
@@ -0,0 +1,31 @@
+"use strict";
+// @ts-ignore
+var path = require("path");
+// @ts-ignore
+var fs = require("fs");
+// @ts-ignore
+var requestPromise = require('request-promise');
+// @ts-ignore
+console.log('Нзвание файла:', path.basename(__filename));
+// @ts-ignore
+var filePath = path.join(__dirname, 'mistakes', 'mistakes.json');
+var options = {
+    method: 'GET',
+    uri: 'https://catalog-errors.readable.w6p.ru/errors/all'
+};
+requestPromise(options)
+    .then(function (response) {
+    // console.log(response,'response')
+    // @ts-ignore
+    fs.writeFile(filePath, response, function (err) {
+        if (err) {
+            console.log(err, 'err creation file');
+        }
+        else {
+            console.log('Файл mistakes создан');
+        }
+    });
+})
+    .catch(function (err) {
+    console.log(err, 'err');
+});
---
+++ src/getErrors.ts
@@ -0,0 +1,32 @@
+// @ts-ignore
+const path = require("path")
+// @ts-ignore
+const fs = require("fs")
+// @ts-ignore
+const requestPromise = require('request-promise')
+
+// @ts-ignore
+console.log('Нзвание файла:', path.basename(__filename));
+// @ts-ignore
+const filePath = path.join(__dirname, 'mistakes', 'mistakes.json')
+
+const options = {
+    method: 'GET',
+    uri: 'https://catalog-errors.readable.w6p.ru/errors/all'
+}
+
+requestPromise(options)
+    .then(function (response: any) {
+        console.log(response,'response')
+        // @ts-ignore
+        fs.writeFile(filePath, response, err => {
+            if(err) {
+                console.log(err,'err creation file');
+            } else {
+                console.log('Файл mistakes создан');
+            }
+        })
+    })
+    .catch(function (err: any) {
+        console.log(err,'err')
+    })
